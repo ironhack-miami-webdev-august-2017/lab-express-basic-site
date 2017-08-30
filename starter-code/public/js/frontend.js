5e4b60ce4a1c07f4453ccb78e5e894b35cbc5ad6
Joao Campos <j.campos4g@gmail.com>
2017-08-30 17:00:33 -0400

    done

---
+++ starter-code/public/js/frontend.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("a").hover(function(){
+    $(this).css("color", "yellow");
+    }, function(){
+    $(this).css("color", "rgba(255,255,255,.7)");
+  });
+
+
+
+});
